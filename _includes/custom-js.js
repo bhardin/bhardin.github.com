b53bc4a5dc0d5c72df402a8f322d0c6f0d88a1fa
Brett Hardin <brett@sourceninja.com>
2012-11-04 07:25:16 -0800

    Making some of the HTML more valid

--- _includes/custom-js.js
+++ _includes/custom-js.js
@@ -1,4 +1,4 @@
-<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.7.2.min.js" type="text/javascript" ></script>
 
 <script type="text/javascript">
 	$(function(){
